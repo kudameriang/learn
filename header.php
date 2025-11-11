d1184cd02f4ba5aeb9174e333fe2883ca2fe6502
kudameriang <financeadan@gmail.com>
2025-11-11 15:06:15 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,4 @@
-<p>
-Halo Imam, selamat datang di Learn!
-</p>
+<p>Halo Imam, selamat datang di Learn</p>
 
 <p>Mau Ke mana?</p> <br/>
 <a href="https://perpustakaan.kuriang.site">Perpustakaan</a> <br/> 
